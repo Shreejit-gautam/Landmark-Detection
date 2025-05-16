81f30794065d985bc5d86d7e0c8456d971f42aa6
Shreejit-gautam <shreejitgautam4@gmail.com>
2025-05-16 23:32:41 +0545

    seeing volex spacing

--- src/main.py
+++ src/main.py
@@ -2,3 +2,5 @@ import SimpleITK as sitk
 input_path = "Original_mask\left_knee.nii"
 #Reading file
 ct = sitk.ReadImage(input_path, sitk.sitkFloat32)
+spacing = ct.GetSpacing()
+print(f"Loaded CT — voxel spacing = {spacing} mm")

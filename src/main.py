b97bb90bbabd79c219a19279095e5efea6f67563
Shreejit-gautam <shreejitgautam4@gmail.com>
2025-05-16 23:37:32 +0545

    two largest componets

--- src/main.py
+++ src/main.py
@@ -14,3 +14,9 @@ bone_mask = sitk.BinaryThreshold(
 
 n_voxels = int(sitk.GetArrayViewFromImage(bone_mask).sum())
 print(f"Thresholded mask contains {n_voxels:,} voxels")
+cc = sitk.ConnectedComponent(bone_mask)
+# Finding Two Largest components
+relabeled = sitk.RelabelComponent(cc, sortByObjectSize=True)
+two_largest = sitk.BinaryThreshold(relabeled, lowerThreshold=1, upperThreshold=2, insideValue=1, outsideValue=0)
+n_after_cc = int(sitk.GetArrayViewFromImage(two_largest).sum())
+print(f"After keeping 2 largest components: {n_after_cc:,} voxels")
